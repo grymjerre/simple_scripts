d9e78acfd88cc596eff5ed6c2e3f779244e2f3af
grymjerre <44071616+grymjerre@users.noreply.github.com>
2020-04-18 13:05:49 +0200

    Add files via upload

---
+++ fizzbuzz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+    for (int i = 1; i <= 100; i++) {
+        if (i%3!=0 && i%5!=0) {
+            printf("%d", i);
+        } else {
+            if (i % 3 == 0)
+                printf("Fizz");
+            if (i % 5 == 0)
+                printf("Buzz");
+        }
+        printf("\n");
+    }
+}
